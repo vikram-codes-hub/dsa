330157b7bcfda4d5820efe14c3ca687e3e452cc4
vikram singh gangwar <vikramsingh9475889367@gmail.com>
2024-11-30 19:11:37 +0530

    heap sort

--- heaps/heap_sort.cpp
+++ heaps/heap_sort.cpp
@@ -17,12 +17,27 @@ void heapify(int arr[],int n,int i){
         heapify(arr,n,largest);
     }
 }
+void heapsort(int arr[],int n){
+    while(n>1){
+        swap(arr[n],arr[1]);
+        n--;
+        heapify(arr,n,1);
+    }
+}
 int main(){
     int arr[6]={-1,54,53,55,52,50};
     int n=5;
     for(int i=n/2;i>0;i--){
         heapify(arr,n,i);
     }
+    cout<<"The elemnts are:";
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    heapsort(arr,n);
+    cout<<"The sorted elements are:";
     for(int i=1;i<=n;i++){
         cout<<arr[i]<<" ";
     }
